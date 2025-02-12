73e53d272293c2969ee3aaa84c289cedbc7a4942
Klytheyy <cestrada@addu.edu.ph>
2025-02-12 09:24:06 +0800

    Update survey.js

--- survey.js
+++ survey.js
@@ -36,7 +36,6 @@ function typeText(element, text, callback) {
 }
 
 function showNextQuestion(index) {
-    console.log("showNextQuestion is running for index:", index);
     if (index >= surveyQuestions.length) {
         document.getElementById('surveyScreens').style.display = 'none';
         document.getElementById('gifContainer').classList.remove('hidden');
@@ -71,7 +70,6 @@ function showMultipleChoiceQuestion(questionObj, index) {
     let answerButtons = document.getElementById('answerButtons');
     answerButtons.innerHTML = "";
 
-    console.log("Setting question text:", questionObj.text);
     questionText.textContent = questionObj.text;
     typeText(questionText, questionObj.text, () => {
         questionObj.options.forEach(option => {
