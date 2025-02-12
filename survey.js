f712c2cf673470ead9b5e75146bd8158bfab3101
Klytheyy <cestrada@addu.edu.ph>
2025-02-12 09:35:45 +0800

    Update survey.js

--- survey.js
+++ survey.js
@@ -1,3 +1,4 @@
+console.log("Survey script loaded!");
 let surveyQuestions = [
     { 
         text: "Which love language do you prefer?", 
@@ -70,7 +71,6 @@ function showMultipleChoiceQuestion(questionObj, index) {
     let answerButtons = document.getElementById('answerButtons');
     answerButtons.innerHTML = "";
 
-    questionText.textContent = questionObj.text;
     typeText(questionText, questionObj.text, () => {
         questionObj.options.forEach(option => {
             let optionId = option.replace(/\s+/g, "-").toLowerCase();
